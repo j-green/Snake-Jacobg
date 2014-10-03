a62b268372874a66270548f7d96a3dc8dbafbd96
Jacob Green <jacobcgreen@verizon.net>
2014-10-03 14:56:38 -0700

    first commit

---
+++ public_html/js/snake.js
@@ -0,0 +1,24 @@
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeigth = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.heigth = screenHeigth;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(180, 250, 213)";
+}
